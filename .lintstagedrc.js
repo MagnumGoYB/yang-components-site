0951eddaff52f67bb11bb4f526661f93ca1e5a56
MagnumGOYB <sosbs@outlook.com>
2023-02-22 17:57:20 +0800

    chore: update .lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,6 +6,6 @@ const buildEslintCommand = (filenames) =>
     .join(' --file ')}`
 
 module.exports = {
-  // '*': 'pnpm pretty',
+  '!(_meta.json)': 'pnpm pretty',
   '*.{js,jsx,ts,tsx}': [buildEslintCommand]
 }
